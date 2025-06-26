795c811c4fb2347dec76b57e55d70a6a59d420d0
unknown <tehnolog3000@gmail.com>
2025-06-26 22:36:40 +0300

    final

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-
 export default defineConfig({
-    plugins: [react()],
     base: '/final_diploma/',
-});
+    plugins: [react()],
+})
